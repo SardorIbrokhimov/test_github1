fd0b3551eb8253f42c4f4a9158006fd1276cec40
fluttercyber <ronaldrask1@gmail.com>
2022-10-22 23:06:28 +0500

    my commit

---
+++ lib/pages2/muhammad_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class MUhammadPAge extends StatefulWidget {
+  const MUhammadPAge({Key? key}) : super(key: key);
+
+  @override
+  State<MUhammadPAge> createState() => _MUhammadPAgeState();
+}
+
+class _MUhammadPAgeState extends State<MUhammadPAge> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
